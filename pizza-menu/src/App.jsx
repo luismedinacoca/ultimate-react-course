f15688975cfa8097d1d9c191f6bf1f9e843fbfd9
Luis Medina Coca <luismedinacoca@gmail.com>
2024-12-06 08:48:32 -0300

    Section 05 Lecture 053: Setting classes and text conditionally

--- pizza-menu/src/App.jsx
+++ pizza-menu/src/App.jsx
@@ -105,13 +105,20 @@ function Menu() {
 
 function Pizza({ pizzaObj }) {
   console.log(pizzaObj);
+
+  //if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? "sold-out" : ""}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3 style={{ color: "green", fontSize: "24px" }}>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>$ {pizzaObj.price}</span>
+        {/* {pizzaObj.soldOut ? (
+          <span>sold out</span>
+        ) : (
+          <span>$ {pizzaObj.price}</span>
+        )} */}
+        <span>{pizzaObj.soldOut ? "SOLD OUT" : "$" + pizzaObj.price}</span>
       </div>
     </li>
   );
