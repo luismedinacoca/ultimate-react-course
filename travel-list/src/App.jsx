4ede14c3d4c63ad9dce67cb6ff8d7e22da2c465b
Luis Medina Coca <luismedinacoca@gmail.com>
2024-12-13 11:01:17 -0300

    Section 06 - Lecture 070: Building the layout

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
+};
 
-export default App
+const Logo = () => {
+  return <h1>🌴 Far Away 🧳</h1>;
+};
+
+const Form = () => {
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 🥰 trip?</h3>
+    </div>
+  );
+};
+
+const PackingList = () => {
+  return <div className="list">LIST</div>;
+};
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>💼 You have X itemson your list, and you already packed X (X%)</em>
+    </footer>
+  );
+};
+
+export default App;
