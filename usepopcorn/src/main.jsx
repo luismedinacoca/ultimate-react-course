c6193039dda01b282f285b8b21ed649705d33bea
luismedinacoca <luismedinacoca@gmail.com>
2025-03-05 10:22:22 -0300

    Section 10 - Lecture 110: Prop Drilling - App only fixed

--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App05 from "./App05.jsx";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App05 />
+    <App />
   </StrictMode>
 );
