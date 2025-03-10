da629338aa43edead255c8ce085df4a836cd77c3
luismedinacoca <luismedinacoca@gmail.com>
2025-03-10 08:31:59 -0300

    Section 10 - Lecture 119: Improving Reusability With Props

--- usepopcorn/src/main.jsx
+++ usepopcorn/src/main.jsx
@@ -3,11 +3,28 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 // import App from "./App.jsx";
 import StarRating from "./StarRating.jsx";
+import { useState } from "react";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </>
+  );
+}
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <StarRating maxRating={5} /> */}
-    <StarRating maxRating={5} />
+    <StarRating size={24} color="red" defaultRating={1} />
+    <StarRating size={32} color="green" defaultRating={3} />
+    <StarRating
+      maxRating={5}
+      messages={["Starting", "In progres", "Okay", "Good", "Amazing"]}
+    />
+
+    <Test />
   </StrictMode>
 );
