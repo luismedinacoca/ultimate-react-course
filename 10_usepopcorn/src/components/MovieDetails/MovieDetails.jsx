b044ea25123687f7c008da8b258089f228a466f7
luismedinacoca <luismedinacoca@gmail.com>
2025-04-04 07:15:27 -0300

    Section 13 - Lecture 153: Adding a New Effect - Changing Page Title

--- 10_usepopcorn/src/components/MovieDetails/MovieDetails.jsx
+++ 10_usepopcorn/src/components/MovieDetails/MovieDetails.jsx
@@ -56,6 +56,12 @@ const MovieDetails = ({ selectedId, onCloseMovie, onAddWatched, watched }) => {
 
     getMovieDetails();
   }, [selectedId]);
+
+  useEffect(() => {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+  }, [title]);
+
   return (
     <div className="details">
       {isLoading ? (
