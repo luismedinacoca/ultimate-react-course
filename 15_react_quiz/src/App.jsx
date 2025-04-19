06d6fe688e3af328b9405face4f0806fd7abe4c2
luismedinacoca <luismedinacoca@gmail.com>
2025-04-19 07:20:59 -0300

    Section 15 - Lecture 187: Yet another Hook - useReducer (Starter)

---
+++ 15_react_quiz/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Section 15 - Lecture 187</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ 15_react_quiz/src/DateCounter.jsx
@@ -0,0 +1,67 @@
+import { useState } from "react";
+
+function DateCounter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  // This mutates the date object.
+  const date = new Date("june 21 2027");
+  date.setDate(date.getDate() + count);
+
+  const dec = () => {
+    // setCount((count) => count - 1);
+    setCount((count) => count - step);
+  };
+
+  const inc = () => {
+    // setCount((count) => count + 1);
+    setCount((count) => count + step);
+  };
+
+  const defineCount = (e) => {
+    setCount(Number(e.target.value));
+  };
+
+  const defineStep = (e) => {
+    setStep(Number(e.target.value));
+  };
+
+  const reset = () => {
+    setCount(0);
+    setStep(1);
+  };
+
+  return (
+    <div className="counter">
+      <div>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={defineStep}
+        />
+        <span>{step}</span>
+      </div>
+
+      <div>
+        <button type="button" onClick={dec}>
+          -
+        </button>
+        <input value={count} onChange={defineCount} />
+        <button type="button" onClick={inc}>
+          +
+        </button>
+      </div>
+
+      <p>{date.toDateString()}</p>
+
+      <div>
+        <button type="button" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+}
+export default DateCounter;
