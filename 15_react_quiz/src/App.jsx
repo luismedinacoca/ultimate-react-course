9fd47d639dc759fbd432af52aa739e2339a4481d
luismedinacoca <luismedinacoca@gmail.com>
2025-04-20 08:07:02 -0300

    Section 16 - Lecture 190: The "React Quiz" App

--- 15_react_quiz/src/App.jsx
+++ 15_react_quiz/src/App.jsx
@@ -1,9 +1,14 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./components/Main";
 
 const App = () => {
   return (
-    <div>
-      <DateCounter />
+    <div className="app">
+      <Header />
+      <Main>
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
     </div>
   );
 };
