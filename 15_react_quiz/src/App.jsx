cbda8326f1a707d18da42e5192ec4b16618efef7
luismedinacoca <luismedinacoca@gmail.com>
2025-04-21 06:55:24 -0300

    Section 16 - Lecture 191: Loading Questions from a Fake API (questions.js - $npm run server)

--- 15_react_quiz/src/App.jsx
+++ 15_react_quiz/src/App.jsx
@@ -1,7 +1,42 @@
 import Header from "./Header";
 import Main from "./components/Main";
+import { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  //status: loading, error, ready, active, finished
+  status: "loading",
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      //.then((data) => console.log(data))
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      //.catch((err) => console.error("Error: " + err));
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
   return (
     <div className="app">
       <Header />
