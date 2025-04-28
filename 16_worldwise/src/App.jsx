a9c6f75183066cf70c1ad14b6c29f63d54fa44e6
Luis Medina <luismedina@Luiss-MacBook-Pro.local>
2025-04-28 07:31:23 -0300

    Section 17 - Lecture 206: Implementing Main Pages and Routes

--- 16_worldwise/src/App.jsx
+++ 16_worldwise/src/App.jsx
@@ -1,10 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import HomePage from './pages/Homepage';
+import Product from './pages/Product';
+import Pricing from './pages/Pricing';
+import PageNotFound from './pages/PageNotFound';
 
 function App() {
 
   return (
-    <>
-      <p>Hello Worldwise!</p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>  
+    </BrowserRouter>
   )
 }
 
