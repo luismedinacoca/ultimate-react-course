7ca8798d1957ca07c9c97d83ddd8a5f308b663b8
Luis Medina Coca <luismedinacoca@gmail.com>
2025-05-02 07:18:06 -0300

    Section 17 - Lecture 212: Nested Routes and Index Route

--- 16_worldwise/src/App.jsx
+++ 16_worldwise/src/App.jsx
@@ -11,11 +11,18 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        {/* <Route path="/" element={<HomePage />} />*/}
+        <Route index element={<HomePage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        {/* Nested Routes */}
+        <Route path="app" element={<AppLayout />} >
+          <Route index element={<p>List of Cities</p>} />
+          <Route path="cities" element={<p>List of Cities 🌟 </p>} />
+          <Route path="countries" element={<p>Countries 🤡 </p>} />
+          <Route path="form" element={<p>Form 💼 </p>} />
+        </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>  
     </BrowserRouter>
--- 16_worldwise/src/components/Sidebar.jsx
+++ 16_worldwise/src/components/Sidebar.jsx
@@ -1,20 +1,25 @@
+import { Outlet } from 'react-router-dom';
 
-import AppNav from './AppNav'
-import Logo from './Logo'
-import styles from './Sidebar.module.css'
+import AppNav from './AppNav';
+import Logo from './Logo';
+import styles from './Sidebar.module.css';
+
 
 const Sidebar = () => {
     return (
       <div className={styles.sidebar}>
         <Logo />
         <AppNav />
+        {/*<p>List of  cities</p>*/}
 
-        <p>List of  cities</p>
+        {/* call the nested routes:  // similar to  {children} props - for cities/countries/form */}
+        <Outlet />
+
         <footer className={styles.footer}>
-          <p className={styles.copyright}>&copy; Copyright {new Date().getFullYear} by WorldWise Inc.</p>
+          <p className={styles.copyright}>&copy; Copyright {new Date().getFullYear()} by WorldWise Inc.</p>
         </footer>
       </div>
   )
 }
 
-export default Sidebar
+export default Sidebar;
