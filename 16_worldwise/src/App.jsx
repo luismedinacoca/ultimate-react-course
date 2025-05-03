3f543dc85d04a2d8e4107e381e4d3c2d5e83c464
Luis Medina Coca <luismedinacoca@gmail.com>
2025-05-03 07:14:23 -0300

    Section 17 - Lecture 213: Implementing the Cities List

--- 16_worldwise/src/App.jsx
+++ 16_worldwise/src/App.jsx
@@ -1,12 +1,36 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { useState, useEffect } from "react";
+
 import HomePage from './pages/Homepage';
 import Product from './pages/Product';
 import Pricing from './pages/Pricing';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
+import CityList from "./components/CityList";
+
+const BASE_URL = "http://localhost:9000"
 
 function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect( () => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`)
+        const data = await res.json();
+        setCities(data);
+      } catch(err) {
+        alert('There was an error loading data...')
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    fetchCities();
+  }, []);
 
   return (
     <BrowserRouter>
@@ -18,8 +42,8 @@ function App() {
         <Route path="login" element={<Login />} />
         {/* Nested Routes */}
         <Route path="app" element={<AppLayout />} >
-          <Route index element={<p>List of Cities</p>} />
-          <Route path="cities" element={<p>List of Cities 🌟 </p>} />
+          <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
+          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
           <Route path="countries" element={<p>Countries 🤡 </p>} />
           <Route path="form" element={<p>Form 💼 </p>} />
         </Route>
