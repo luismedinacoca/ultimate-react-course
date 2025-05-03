3227ffe85408d49bb280c6dc18e07ed983413902
Luis Medina Coca <luismedinacoca@gmail.com>
2025-05-03 07:50:21 -0300

    Section 17 - Lecture 214: Implementing the Countries List

--- 16_worldwise/src/App.jsx
+++ 16_worldwise/src/App.jsx
@@ -8,6 +8,7 @@ import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './pages/AppLayout';
 import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
 
 const BASE_URL = "http://localhost:9000"
 
@@ -44,7 +45,7 @@ function App() {
         <Route path="app" element={<AppLayout />} >
           <Route index element={<CityList cities={cities} isLoading={isLoading}/>} />
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
-          <Route path="countries" element={<p>Countries 🤡 </p>} />
+          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
           <Route path="form" element={<p>Form 💼 </p>} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
