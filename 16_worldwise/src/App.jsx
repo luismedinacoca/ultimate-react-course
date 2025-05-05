d095b1bb6018d09e7afbd4f235eefa4cd2dc472d
Luis Medina Coca <luismedinacoca@gmail.com>
2025-05-05 08:02:29 -0300

    Section 17 - Lecture 218: Programmatic Navigation with useNavigate

--- 16_worldwise/src/App.jsx
+++ 16_worldwise/src/App.jsx
@@ -10,6 +10,7 @@ import AppLayout from './pages/AppLayout';
 import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
+import Form from "./components/Form";
 
 const BASE_URL = "http://localhost:9000"
 
@@ -47,7 +48,7 @@ function App() {
           <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
           <Route path="cities/:id" element={<City />} />
           <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
-          <Route path="form" element={<p>Form 💼 </p>} />
+          <Route path="form" element={<Form />} />
         </Route>
         <Route path="*" element={<PageNotFound />} />
       </Routes>  
