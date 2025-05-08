2a8bba16e594bd12c1550df15c876df34ec80b07
Luis Medina Coca <luismedinacoca@gmail.com>
2025-05-08 08:23:57 -0300

    Section 18 - Lecture 225: Advanced Pattern - A Custom Provider and Hook

--- 17_atomic_blog/src/App-v1.jsx
+++ 17_atomic_blog/src/App-v1.jsx
@@ -1,19 +1,17 @@
 import { createContext, useContext, useEffect, useState } from "react";
-//import { PostProvider, PostContext } from './components/PostProvider';
-import { PostProvider, usePosts } from './components/PostProvider';
-//import { faker } from "@faker-js/faker";
+import { faker } from "@faker-js/faker";
 
-/*
 function createRandomPost() {
   return {
     title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
     body: faker.hacker.phrase(),
   };
 }
-*/
+
+// 1) 🌟 CREATE A NEW CONTEXT 🌟
+const PostContext = createContext();
 
 function App() {
-  /*
   const [posts, setPosts] = useState(() =>
     Array.from({ length: 30 }, () => createRandomPost())
   );
@@ -37,14 +35,6 @@ function App() {
   function handleClearPosts() {
     setPosts([]);
   }
-  */
-
-  /*
-  const x = usePosts();
-  console.log(x);
-  */  // throw new Error('PostContext was used outside of the PostProvider')
-  
-  const [isFakeDark, setIsFakeDark] = useState(false);
 
   // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
   useEffect(
@@ -55,28 +45,42 @@ function App() {
   );
 
   return (
-    <section>
-      <button
-        onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
-        className="btn-fake-dark-mode"
-      >
-        {isFakeDark ? "☀️" : "🌙"}
-      </button>
-        <PostProvider>
-          <Header />
-          <Main />
-          <Archive />
-          <Footer />
-        </PostProvider>  
-    </section>
+    // 2) 🌟 PROVIDE VALUE TO CHILD COMPONENTS 🌟
+    <PostContext.Provider value={ 
+      {
+        post: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      } 
+    }>
+      <section>
+        <button
+          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+          className="btn-fake-dark-mode"
+        >
+          {isFakeDark ? "☀️" : "🌙"}
+        </button>
+
+        <Header
+          posts={searchedPosts}
+          onClearPosts={handleClearPosts}
+          searchQuery={searchQuery}
+          setSearchQuery={setSearchQuery}
+        />
+        <Main posts={searchedPosts} onAddPost={handleAddPost} />
+        <Archive onAddPost={handleAddPost} />
+        <Footer />
+      </section>
+    </PostContext.Provider>
   );
 }
 
 {/*function Header({ posts, onClearPosts, searchQuery, setSearchQuery }) {*/}
 function Header() {
   // 3) Consuming the context Value:
-  //const { onClearPosts } = useContext(PostContext);
-  const { onClearPosts } = usePosts();
+  const { onClearPosts } = useContext(PostContext);
   return (
     <header>
       <h1>
@@ -98,8 +102,7 @@ function Header() {
 
 {/*function SearchPosts({ searchQuery, setSearchQuery }) {*/}
 function SearchPosts() {
-  //const { searchQuery, setSearchQuery } = useContext(PostContext);
-  const { searchQuery, setSearchQuery } = usePosts();
+  const { searchQuery, setSearchQuery } = useContext(PostContext);
   return (
     <input
       value={searchQuery}
@@ -112,8 +115,7 @@ function SearchPosts() {
 {/*function Results({ posts }) {*/}
 function Results() {
   // 3) 🌟 CONSUMING THE CONEXT VALUE 🌟
-  //const { posts } = useContext(PostContext);
-  const { posts } = usePosts();
+  const { posts } = useContext(PostContext);
 
   //return <p>🚀 {posts.length} atomic posts found</p>;
   return (
@@ -135,9 +137,9 @@ function Main() {
 
 //function Posts({ posts }) {
 function Posts() {
+
   // 3) 🌟 CONSUMING THE CONEXT VALUE 🌟
-  //const { posts } = useContext(PostContext);
-  const { posts } = usePosts();
+  const { post } = useContext(PostContext);
   return (
     <section>
       <List posts={posts} />
@@ -151,8 +153,7 @@ function FormAddPost() {
   const [body, setBody] = useState("");
 
   // 3) 🌟 CONSUMING THE CONEXT VALUE 🌟
-  //const { onAddPost } = useContext(PostContext);
-  const { onAddPost } = usePosts();
+  const { onAddPost } = useContext(PostContext);
 
   const handleSubmit = function (e) {
     e.preventDefault();
@@ -183,8 +184,7 @@ function FormAddPost() {
 function List() {
 
   // 3) 🌟 CONSUMING THE CONEXT VALUE 🌟
-  //const { posts } = useContext(PostContext);
-  const { posts } = usePosts();
+  const { posts } = useContext(PostContext);
   return (
     <ul>
       {posts.map((post, i) => (
@@ -202,8 +202,7 @@ function Archive() {
   // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
 
   // 3) 🌟 CONSUMING THE CONEXT VALUE 🌟
-  //const { onAddPost, createRandomPost } = useContext(PostContext);
-  const { onAddPost, createRandomPost } = usePosts();
+  const { onAddPost } = useContext(PostContext);
   
   const [posts] = useState(() =>
     // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
