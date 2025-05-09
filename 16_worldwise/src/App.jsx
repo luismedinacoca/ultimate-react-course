6d6302c0eb427d187a1eb09168cf994bb1df2ed2
Luis Medina Coca <luismedinacoca@gmail.com>
2025-05-09 11:15:00 -0300

    Section 18 - Lecture 227: Back to "Worldwise" - creating a CitiesContext

--- 16_worldwise/src/App.jsx
+++ 16_worldwise/src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
-import { useState, useEffect } from "react";
 
 import HomePage from './pages/Homepage';
 import Product from './pages/Product';
@@ -11,10 +10,12 @@ import CityList from "./components/CityList";
 import CountryList from "./components/CountryList";
 import City from "./components/City";
 import Form from "./components/Form";
+import { CitiesProvider } from "./contexts/CitiesContext";
 
-const BASE_URL = "http://localhost:9000"
+//const BASE_URL = "http://localhost:9000"
 
 function App() {
+/*
   const [cities, setCities] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
@@ -34,28 +35,30 @@ function App() {
 
     fetchCities();
   }, []);
+*/
+
 
   return (
-    <BrowserRouter>
-      <Routes>
-        {/* <Route path="/" element={<HomePage />} />*/}
-        <Route index element={<HomePage />} />
-        <Route path="pricing" element={<Pricing />} />
-        <Route path="product" element={<Product />} />
-        <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} >
-          {/*<Route index element={<CityList cities={cities} isLoading={isLoading}/>} />*/}
-          {/* //It navigates to Cities as expected however the back button from menu does not work.
-          <Route index element={<Navigate to="cities" />} />*/}
-          <Route index element={<Navigate replace to="cities" />} /> {/* declarative way! */}
-          <Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />
-          <Route path="cities/:id" element={<City />} />
-          <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
-          <Route path="form" element={<Form />} />
-        </Route>
-        <Route path="*" element={<PageNotFound />} />
-      </Routes>  
-    </BrowserRouter>
+    <CitiesProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<HomePage />} />
+          <Route path="pricing" element={<Pricing />} />
+          <Route path="product" element={<Product />} />
+          <Route path="login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} >
+            <Route index element={<Navigate replace to="cities" />} />
+            {/*<Route path="cities" element={<CityList cities={cities} isLoading={isLoading}/>} />*/}
+            <Route path="cities" element={<CityList />} />
+            <Route path="cities/:id" element={<City />} />
+            {/*<Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />*/}
+            <Route path="countries" element={<CountryList />} />
+            <Route path="form" element={<Form />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>  
+      </BrowserRouter>
+    </CitiesProvider>
   )
 }
 
