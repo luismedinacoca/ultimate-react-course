f4c13f3fee073a1ca27dd6184028bdbf105f07a0
Luis Medina Coca <luismedinacoca@gmail.com>
2025-06-23 07:21:38 -0300

    Section 18 - Lecture 239: Adding Fake Authentication - Implementing "Login"

--- 16_worldwise/src/contexts/FakeAuthContext.jsx
+++ 16_worldwise/src/contexts/FakeAuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const AuthContext = createContext();
 
@@ -54,6 +54,7 @@ function AuthProvider({children }){
 function useAuth(){
   const context = useContext(AuthContext);
   if(context === undefined) throw new Error("AuthContext was used outside AuthProvider");
+  return context;
 }
 
 export { AuthProvider, useAuth }
