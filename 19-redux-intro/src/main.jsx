61b23a39e736503642371d919c7e25e8884f0090
Luis Medina Coca <luismedinacoca@gmail.com>
2025-07-21 06:50:22 -0300

    Section 20 - lecture 267:  Baack to React - Connecting our Redux App with React

--- 19-redux-intro/src/main.jsx
+++ 19-redux-intro/src/main.jsx
@@ -1,15 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import App from './App.jsx';
 import store from'./store.js';
 
+import './index.css';
+
 //store.dispatch(deposit(500));
-store.dispatch({type: "account/deposit", payload: 500});
-console.log(store.getState());
+//store.dispatch({type: "account/deposit", payload: 500});
+//console.log(store.getState());
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
