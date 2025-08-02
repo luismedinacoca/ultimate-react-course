5a88c141c6f55e5a50c431367a252adca738db9a
Luis Medina Coca <luismedinacoca@gmail.com>
2025-08-02 07:17:29 -0300

    Section 20 - Lecture 272: The Redux DevTools

--- 19-redux-intro/src/store.js
+++ 19-redux-intro/src/store.js
@@ -1,4 +1,5 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
+import { composeWithDevTools } from '@redux-devtools/extension';
 import {thunk} from "redux-thunk";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
@@ -9,6 +10,6 @@ const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
 });
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
 console.log("hey Redux!");
