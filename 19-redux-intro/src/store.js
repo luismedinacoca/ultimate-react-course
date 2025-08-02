479f5680d680470cfcd3058ee361378a18a63ce1
Luis Medina Coca <luismedinacoca@gmail.com>
2025-08-02 07:51:09 -0300

    Section 20 - Lecture 274: Creating the Store with Redux Toolkit

--- 19-redux-intro/src/store.js
+++ 19-redux-intro/src/store.js
@@ -1,11 +1,19 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import { composeWithDevTools } from '@redux-devtools/extension';
-import {thunk} from "redux-thunk";
+//import {applyMiddleware, combineReducers, createStore} from "redux";
+//import { composeWithDevTools } from '@redux-devtools/extension';
+//import {thunk} from "redux-thunk";
+import { configureStore } from "@reduxjs/toolkit";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
+const store = configureStore({
+  reducer: {
+    account: accountReducer,
+    customer: customerReducer,
+  },
+});
+export default store;
 
-//const store = createStore(accountReducer);
+/*
 const rootReducer = combineReducers({
   account: accountReducer,
   customer: customerReducer,
@@ -13,3 +21,4 @@ const rootReducer = combineReducers({
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
 export default store;
 console.log("hey Redux!");
+*/
