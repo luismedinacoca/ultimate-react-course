b62ba4a3f446f5395f156d6bbd58c49658d4fc16
Luis Medina Coca <luismedinacoca@gmail.com>
2025-08-07 07:55:16 -0300

    Section 20 - Lecture 277: Creating the Customer Slice

--- 19-redux-intro/src/features/customers/customerSlice.js
+++ 19-redux-intro/src/features/customers/customerSlice.js
@@ -1,3 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  fullName: "",
+  nationalID: "",
+  createdAt: "",
+};
+
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID){
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          }
+        }
+      },
+      reducer(state, action){
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = action.payload.createdAt;
+      }
+    },
+    updateName(state, action){
+      state.fullName = action.payload;
+    }
+  }
+})
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/*
+const customerSlice = createSlice({
+
 const initialStateCustomer = {
   fullName: "",
   nationalID: "",
@@ -22,7 +63,7 @@ export default function customerReducer(state = initialStateCustomer, action) {
   }
 }
 
-/*********** 5. Create a Customer ***********/
+/*********** 5. Create a Customer *********** /
 export function createCustomer(fullName, nationalID){
   return {
     type: "customer/createCustomer", 
@@ -36,9 +77,11 @@ export function createCustomer(fullName, nationalID){
 //store.dispatch(createCustomer("John Doe", "1234567890"));
 //console.log(store.getState());
 
-/*********** 6. Update Customer ***********/
+/*********** 6. Update Customer *********** /
 export function updateName(fullName){
   return {type: "customer/updateName", payload: fullName}
 }
 //store.dispatch(updateName("Jonas schmedtmann"));
 //console.log(store.getState());
+
+*/
