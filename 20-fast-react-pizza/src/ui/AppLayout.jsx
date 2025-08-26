ff5be17100a65c6fc6206711172d09eb94516cc9
Luis Medina Coca <luismedinacoca@gmail.com>
2025-08-26 07:28:42 -0300

    Section 22 - Lecture 288: Displaying a Loading Indicator

--- 20-fast-react-pizza/src/ui/AppLayout.jsx
+++ 20-fast-react-pizza/src/ui/AppLayout.jsx
@@ -1,20 +1,26 @@
-import Header from "./Header"
-import CartOverview from "../features/cart/CartOverview"
-import { Outlet } from "react-router-dom"
+import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import { Outlet, useNavigation } from "react-router-dom";
+import Loader from "./Loader";
 
 const AppLayout = () => {
-  return (
-    <div>
-      <Header />
-			<main>
-				<h1>AppLayout Content</h1>
-				<p>************************************************</p>
-				<Outlet />
-				<p>************************************************</p>
-			</main>
-			<CartOverview />
-    </div>
-	)
-}
+  const navigation = useNavigation();
+  console.log(navigation);
+  const isLoading = navigation.state === "loading";
 
-export default AppLayout
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+      <Header />
+      <main>
+        <h1>AppLayout Content</h1>
+        <p>************************************************</p>
+        <Outlet />
+        <p>************************************************</p>
+      </main>
+      <CartOverview />
+    </div>
+  );
+};
+
+export default AppLayout;
